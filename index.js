e29ac979b641972a8ae699b335ade37231544cf9
bayuwonderful <124481662+bayuwonderful@users.noreply.github.com>
2023-02-22 18:52:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
+var { color } = require('./lib/color')
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
